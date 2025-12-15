f8d2d0f69fe835116c3ff2a36eb2a42a4de5f75e
eager4morecode <115359627+eager4morecode@users.noreply.github.com>
2025-12-15 11:09:58 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,49 @@
+output "vpc_id" {
+  description = "ID of the created VPC."
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the created VPC."
+  value       = aws_vpc.this.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets."
+  value       = [for s in aws_subnet.public : s.id]
+}
+
+output "public_subnet_cidrs" {
+  description = "CIDR blocks of the public subnets."
+  value       = [for s in aws_subnet.public : s.cidr_block]
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets."
+  value       = [for s in aws_subnet.private : s.id]
+}
+
+output "private_subnet_cidrs" {
+  description = "CIDR blocks of the private subnets."
+  value       = [for s in aws_subnet.private : s.cidr_block]
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway."
+  value       = aws_internet_gateway.this.id
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of the NAT gateways (if created)."
+  value       = aws_nat_gateway.this[*].id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table."
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "ID of the private route table."
+  value       = aws_route_table.private.id
+}
